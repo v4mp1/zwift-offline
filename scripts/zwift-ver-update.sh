cd8171601c42c6acc69adbe092b0a0b7bac009eb
v4mp1 <vampy2k5@gmail.com>
2021-09-24 14:14:12 +0200

    Changes

--- scripts/zwift-ver-update.sh
+++ scripts/zwift-ver-update.sh
@@ -2,6 +2,3 @@
 cd /home/pi/zwift-offline-master/cdn/gameassets
 curl -O https://cdn.zwift.com/gameassets/ZwiftAndroid_ver_cur.xml
 curl -O https://cdn.zwift.com/gameassets/ZwiftiOS_ver_cur.xml
-#cd /home/pi/zwift-offline-master/cdn/gameassets/Zwift_Updates_Root
-#curl -O https://cdn.zwift.com/gameassets/Zwift_ver_cur.xml
-#curl -O https://cdn.zwift.com/gameassets/ZwiftMac_ver_cur.xml
